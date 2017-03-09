1dffebd1b30f03d266ad04ec7614a19458095649
Mihail <mihailkovalevskiy@gmail.com>
2017-03-09 20:24:06 +0700

    Lesson 1.v.4.0

--- lessons/lesson1/task/l1-trigon-rectangular.rb
+++ lessons/lesson1/task/l1-trigon-rectangular.rb
@@ -12,20 +12,23 @@ trigon_array = [a,b,c]
 
 gipotenuse = trigon_array.max
 
-trigon_array.delete(trigon_array.max)
+trigon_array.delete(gipotenuse)
 
 if trigon_array.count == 0
   puts "Все 3 стороны равны. Треугольник равнобедренный и равносторонний, но не прямоугольный"
 end
 
-if gipotenuse == trigon_array[0] || gipotenuse == trigon_array[1]
-  puts "Треугольник равнобедренный"
+if trigon_array.count ==1
+  puts "Такое невозможно. Гипотенуза не может быть равна катету"
 else
-  puts "Треугольник не равнобедренный"
-end
-
-if gipotenuse ** 2 == trigon_array[0] ** 2 + trigon_array[1] ** 2
-  puts "Треугольник прямоугольный"
-else
-  puts "Треугольник не прямоугольный"
+  if trigon_array[0] == trigon_array[1]
+    puts "Треугольник равнобедренный"
+  else
+    puts "Треугольник не равнобедренный"
+  end
+  if gipotenuse ** 2 == trigon_array[0] ** 2 + trigon_array[1] ** 2
+    puts "Треугольник прямоугольный"
+  else
+    puts "Треугольник не прямоугольный"
+  end
 end
