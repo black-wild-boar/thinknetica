cbb1542d2d5886eb670a8d0147a8b7ebfe0a70e1
mihailkovalevskiy <mihailkovalevskiy@gmail.com>
2017-06-06 17:57:16 +0700

    Lesson 9.task_tmp

--- lessons/lesson9/task/accessors.rb
+++ lessons/lesson9/task/accessors.rb
@@ -1,30 +1,33 @@
-Написать модуль Acessors, содержащий следующие методы, которые можно вызывать на
-уровне класса:
+# Написать модуль Acessors, содержащий следующие методы, которые можно вызывать на
+# уровне класса:
 
-метод   
-attr_accessor_with_history
+# метод   
+# attr_accessor_with_history
  
- Этот метод динамически создает геттеры и сеттеры для любого кол-ва атрибутов,
-при этом сеттер сохраняет все значения инстанс-переменной при изменении этого
-значения.  Также в класс, в который подключается модуль должен добавляться
-инстанс-метод   <имя_атрибута>_history
+#  Этот метод динамически создает геттеры и сеттеры для любого кол-ва атрибутов,
+# при этом сеттер сохраняет все значения инстанс-переменной при изменении этого
+# значения.  Также в класс, в который подключается модуль должен добавляться
+# инстанс-метод   <имя_атрибута>_history
  
-  который возвращает массив всех значений данной переменной.
+#   который возвращает массив всех значений данной переменной.
 
-метод  
-strong_attr_acessor
+# метод  
+# strong_attr_acessor
  
- который принимает имя атрибута и его класс. При этом создается геттер и
-сеттер для одноименной инстанс-переменной, но сеттер проверяет тип
-присваемоего значения. Если тип отличается от того, который указан вторым
-параметром, то выбрасывается исключение. Если тип совпадает, то значение
-присваивается. 
+#  который принимает имя атрибута и его класс. При этом создается геттер и
+# сеттер для одноименной инстанс-переменной, но сеттер проверяет тип
+# присваемоего значения. Если тип отличается от того, который указан вторым
+# параметром, то выбрасывается исключение. Если тип совпадает, то значение
+# присваивается. 
 
 module Acessors
 
-  def attr_accessor_with_history(method_name, *args)
-
-    send(method_name, instance_variable_set(args))
+  def attr_accessor_with_history(*methods_name)
+    methods_name.each do |method_name|
+      variable_name = "@#{method_name}".to_sym
+      define_method(method_name.to_sym) {instance_variable_get(variable_name)}
+      define_method("#{method_name}=".to_sym) { |value| instance_variable_set(variable_name, value) }
+    end
   #call on Сlass level     #send
   end
 
@@ -32,3 +35,41 @@ module Acessors
   end
 
 end
+
+class CreateAcessor
+  extend Acessors
+
+  # def add(name)
+  @@methods = []
+  def initialize#(name)
+    @@methods << self
+  end
+
+  def method_missing(method_name)
+    
+  end
+
+  def add_accessor
+   loop do
+     p 'enter method name'
+     method_name = gets.chomp.to_sym
+    if method_name == :exit
+      puts @methods.inspect    
+      break 
+    end
+    attr_accessor_with_history method_name
+
+    # attr_accessor_with_history name
+   end
+   end
+end
+
+#p 'print'
+#n = gets.chomp
+#n = CreateAcessor.new
+#p n.public_methods
+#n.add('m1')
+#p n
+
+#c1.public_methods
+#c1.instance_variables
