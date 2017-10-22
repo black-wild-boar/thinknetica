383e8e8eff830574bdc06cf9f21bcce190897967
Mihail <mihailkovalevskiy@gmail.com>
2017-10-23 01:37:38 +0700

    Lesson 9.2.v.1.2.alfa

--- lessons/lesson9/task/modules/l9validation.rb
+++ lessons/lesson9/task/modules/l9validation.rb
@@ -1,9 +1,29 @@
 module Validation
   module ClassMethods
 
-    # CHECKS = { presence: ->(name) { presence(name) },
+    # CHECKS = { validate_presence: ->(name) { validate_presence(name) },
     #           format: ->(name, format) { format(name, format) },
     #           type: ->(name, type) { type(name ,type)} }
+
+    CHECKS = { presence: proc {|name| validate_presence(name) },
+              format: proc { |name, format| validate_format(name, format) },
+              type: proc { |name, type| validate_type(name ,type)}
+            }
+
+    def validate_new(check_attr, check_type, attrs)
+      p 'class method validate_new'
+      
+      p "check_attr #{check_attr}"
+      p "check_type #{check_type}"
+
+
+      CHECKS[check_type].call(check_attr)
+
+      # p self
+      # p instance_variables
+      # @@checks[attrs[:validate_type]].call
+      # obj, name, check_type, *attrs)    
+    end
 
     def validate(obj, name, check_type, *attrs)
       # где правильнее вычислять значение: в публичном или приватном методе?
@@ -11,7 +31,7 @@ module Validation
       name = obj.instance_variable_get("@#{name}".to_sym)
       if attrs.empty?
         # CHECKS[check_type].call(name)
-        presence(name) if check_type == :presence
+        validate_presence(name) if check_type == :validate_presence
       else
         attrs.each do | attr |
           format(name, attr) if check_type == :format
@@ -33,17 +53,18 @@ module Validation
       p false
     end
 
- private 
+ # private 
 
-    def presence(name)
-      p 'Check presence...'
+    def validate_presence(name)
+      p 'Check validate_presence...'
+      p name
       # почему не работает вариант с тернарными операторами?
       # valid? ? !(value.nil? || value == '') : !valid?
       
       raise if name.nil? || name == ''
-      p 'No presence error'
+      p 'No validate_presence error'
       rescue
-      p "Presence validation error! #{false}"
+      p "validate_presence validation error! #{false}"
 
       # p valid?
       # true
@@ -56,8 +77,8 @@ module Validation
       
     end
 
-    def format(name, format)# = /^[A-Z]{0,3}$/)
-      p 'Check format...'
+    def validate_format(name, format)# = /^[A-Z]{0,3}$/)
+      p 'Check validate_format...'
       format = Regexp.new "#{format}"
       
       raise 'Format validation error!' if name !~ format
@@ -66,8 +87,8 @@ module Validation
       p "Format validation error! #{false}"
     end
 
-    def type(name ,type)
-      p 'Check type'
+    def validate_type(name ,type)
+      p 'Check validate_type'
       raise 'Type validateion error' if name.class.to_s != type.to_s.capitalize
       p 'No type error'
       rescue
@@ -76,8 +97,22 @@ module Validation
   end
 
   module InstanceMethods
-    def validate!(name, check_type, *attrs)
-      self.class.valid?(obj = self, name, check_type, *attrs)
+    def validate!(check_attr, check_type, *attrs)
+      # self.class.valid?(obj = self, name, check_type, *attrs)
+      p 'instance method validate!'
+      p "check_attr #{check_attr}"
+      p "check_attr value #{instance_variable_get("@#{check_attr}".to_sym)}"
+
+      p "check_type #{check_type}"
+      check_attr = instance_variable_get("@#{check_attr}".to_sym)
+p "check_attr #{check_attr}"
+
+      self.class.send(:validate_new, check_attr, check_type, attrs)
     end
+
+      def validate_presence(name)
+      p 'Check validate_presence...'
+      p name
+      end
   end
 end
