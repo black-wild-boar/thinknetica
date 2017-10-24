56d3eff0e44ed76b7c4d834f4089542e2dde7d18
black-wild-boar <mihailkovalevskiy@gmail.com>
2017-10-24 18:50:40 +0700

    Lesson 9.2.v.1.5validateion_hash_rules.rb

--- lessons/lesson9/task/modules/l9validation.rb
+++ lessons/lesson9/task/modules/l9validation.rb
@@ -1,47 +1,114 @@
 module Validation
   module ClassMethods
     def validate(check_attr, check_type, *attrs)
-      p 'Class method validate'
-      #сохраняем правила валидации
-      # @validate_rules = { check_attr: {presence: attrs,format: attrs, type: attrs} }
-            
-      value = {name: check_attr, attrs: attrs}
-      var_name = "@#{check_type}".to_sym
-      
-      instance_variable_set(var_name, value)
-      p '7777'
-      # p instance_variables
-      # p var_name
-      # p instance_variable_get(var_name)
+      p 'Save check rules invoke from class'
 
+      # main_hash = {check_attr: {presence: *attrs, type: *attrs, format: *attrs} }
+      # т.к. может быть несколько разных проверок на инстанс переменной
+
+
+                # var_name = "@#{check_attr}".to_sym
+                # value = {check_type: check_type, attrs: attrs}
+                
+                # # p "value #{value}"
+                # if instance_variable_get(var_name).nil?
+                #   instance_variable_set(var_name, value)
+                #  else
+                #    value_prev = instance_variable_get(var_name)
+                #    # p "value_prev = #{value_prev}"
+                #    instance_variable_set(var_name, value_prev << value)
+                # end
+
+      # var_name = "@#{check_attr}".to_sym
+
+      # value_prev = [] << instance_variable_get(var_name)
+
+      # value = {check_type: check_type, attrs: attrs}
+
+      # instance_variable_set(var_name, value_prev << value)
+      
+ 
+ # p "instance_variables #{instance_variables}"
+ # p "instance_variable_get(:@name) #{instance_variable_get(:@name)}"
+ # p "instance_variable_get(:@name1) #{instance_variable_get(:@name1)}"
+
+
+    var_name = "@#{check_attr}".to_sym
+    p "var_name = #{var_name}"
+    # value = [] << {"#{check_attr}": {check_type: check_type, attrs: attrs}}
+    
+    value = {"#{check_attr}": [ check_type: check_type, attrs: attrs] }
+    p "value = #{value}"
+    # p "value[to_sym] = #{value["#{check_attr}".to_sym]}"
+    p value.fetch("#{check_attr}".to_sym)
+    
+    if !instance_variable_get(var_name).nil?
+      global_scope = instance_variable_get(var_name)
+
+    p "value = #{value}"
+    p "value[to_sym] = #{value["#{check_attr}".to_sym]}"
+    p value.fetch("#{check_attr}".to_sym)
+
+
+      p "global_scope = #{global_scope}"
+
+p "global_scope = #{global_scope["#{check_attr}".to_sym]}"
+      global_scope = "#{global_scope["#{check_attr}".to_sym]}"
+      p "global_scope = #{global_scope}"
+      value = "#{value["#{check_attr}".to_sym]}"
+    p "value = #{value}"
+
+      instance_variable_set(var_name, global_scope << value)
+    else
+      # run one first time
+      instance_variable_set(var_name, value)
+    end
+
+    p "instance_variables #{instance_variables}"
+    vars = instance_variables #{instance_variables}
+    vars.each do | var |
+      p "var = #{var} "
+      p instance_variable_get(var)
+    end
     end
   end
 
   module InstanceMethods
-    def validate!(check_attr, check_type, *attrs)
+    def validate!
       p 'Instance method validate!'
-      # check_attr = instance_variable_get("@#{check_attr}".to_sym)
-      # self.send("validate_#{check_type}".to_sym, check_attr, attrs)
-      p '555555'
-      # p self
-      # p check_type
-      # p instance_variable_get("@#{check_attr}".to_sym)
-      #from validate scheme
-      check_hash_value = self.class.instance_variable_get("@#{check_type}".to_sym)
-      p check_hash_value
-      p check_hash_value.class
-      p check_hash_value.keys
-      #from validate scheme
-      check_attr = check_hash_value[:name]
+      check_attr = self.class.send(:instance_variables)
+p '5555'
       p check_attr
-      attrs = check_hash_value[:attrs]
-      p attrs
-      self.send("validate_#{check_type}".to_sym, check_attr, attrs)
+      # p self.class.send(:instance_variable_get, :@name)
+      # p self.class.send(:instance_variable_get(:@name1))
+
+      check_attr.each do | key |
+        p 'each'
+        p key
+        # check_type = key.to_s.delete('@')
+        check_attr_key = self.class.send(:instance_variable_get, key)#[:check_type]
+        p check_attr_key
+        # check_attr_key = self.class.send(:instance_variable_get, key)[:check_type]
+        # p check_attr_key
+        # check_attr = instance_variable_get(key)
+        # p check_attr
+
+      end
+
+
+#       check_attr.each do | key |
+#         check_type = key.to_s.delete('@')
+#         check_attr_key = self.class.send(:instance_variable_get, key)[:check_attr]
+#         check_attr = instance_variable_get("@#{check_attr_key}".to_sym)
+# p check_attr
+#         attrs = self.class.send(:instance_variable_get, key)[:attrs]
+#         self.send("validate_#{check_type}".to_sym, check_attr, attrs)
+#       end
     end
 
   private
 
-    def validate_presence(name, attr_nil=nil)
+    def validate_presence(name, attr=nil)
       p 'Check validate_presence...'
       raise 'Presence validation error!' if name.nil? || name == ''
         p "No validate_presence error: #{valid?}"
@@ -49,18 +116,18 @@ module Validation
         p "Validate_presence validation error! #{!valid?}"
     end
 
-    def validate_format(name, format)
+    def validate_format(name, attr)
       p 'Check validate_format...'
-      format = Regexp.new(format[0])
-      raise 'Format validation error!' if name !~ format
+      attr = Regexp.new(attr[0])
+      raise 'Format validation error!' if name !~ attr
       p "No format error: #{valid?}"
     rescue
       p "Format validation error! #{!valid?}"
     end
 
-    def validate_type(name ,type)
+    def validate_type(name ,attr)
       p 'Check validate_type'
-      raise 'Type validation error' if !name.is_a?type[0]
+      raise 'Type validation error' if !name.is_a?attr[0]
       p "No type error: #{valid?}"
     rescue
       p "Type validation error! #{!valid?}"
@@ -79,21 +146,16 @@ class Test
   attr_accessor :name, :name1
 
   validate :name1, :presence
-  validate :name1, :format, /[A-Z]/
   validate :name1, :type, String
+  validate :name, :type, Integer
+  validate :name, :presence
+  validate :name1, :format, '[A-Z]'
+
 end
 
-p 'b instance of class A. check_attr :name1'
+p 'Variable b contains an instance of class Test. Check attributes :name1 && :name with rules invoke from class'
 b = Test.new
 b.name1 = 'DD'
+b.name = 5
 p b 
-b.validate!(:name1, :presence)
-# b.validate!(:name1, :format, /[A-Z]/)
-# b.validate!(:name1, :type, String)
-
-# p 'b instance of class A. check_attr :name'
-# b.name = ''
-# p b
-# b.validate!(:name, :presence)
-# b.validate!(:name, :format, /a-z/)
-# b.validate!(:name, :type, Integer)
+b.validate!
